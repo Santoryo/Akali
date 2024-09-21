3ea69259b04f060769877eb94932fc9475759418
sant <santoryo.evergarden@gmail.com>
2024-09-21 16:56:59 +0200

    sdsda

--- index.ts
+++ index.ts
@@ -10,6 +10,7 @@ import { treasureRealmsFetch } from "./modules/tft/treasureRealms";
 import { tftBattlePassFetch } from "./modules/tft/battlePass";
 import { storeSaleFetch } from "./modules/storeSale";
 dotenv.config();
+import { exec } from 'child_process';
 
 const hook = new Webhook(process.env.WEBHOOK_URL);
 
@@ -21,16 +22,28 @@ const agent = new Agent({
 
 setGlobalDispatcher(agent);
 
-try {
-  const credentials = await authenticate();
-  await eventsFetch(credentials);
-  await treasureRealmsFetch(credentials);
-  await tftBattlePassFetch(credentials);
-  await storeSaleFetch(credentials);
-  await mythicShopFetch(credentials);
-} catch (error) {
-  hook.send("**[Akali]**: " + error);
-  console.error(error);
+while(true)
+{
+  try {
+    const credentials = await authenticate();
+    await eventsFetch(credentials);
+    await treasureRealmsFetch(credentials);
+    await tftBattlePassFetch(credentials);
+    await storeSaleFetch(credentials);
+    await mythicShopFetch(credentials);
+    process.exit(0);
+  } catch (error) {
+    console.log("League client not found, retrying in 10 seconds...");
+    exec('"F:\\Riot Games\\Riot Client\\RiotClientServices.exe" --launch-product=league_of_legends --launch-patchline=live', (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+      console.error(`stderr: ${stderr}`);
+    });
+  }
+  await new Promise(resolve => setTimeout(resolve, 10000));
 }
 
 // await yourShopFetch(credentials);
